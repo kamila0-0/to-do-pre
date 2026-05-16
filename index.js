8faa5a6c14bd9a54c9b4bf15b1ca3055ac54e4d1
Камила <kkhanova@sfedu.ru>
2026-05-16 16:34:39 +0300

    удаление задач

--- index.js
+++ index.js
@@ -23,10 +23,17 @@ function createItem(item) {
 	const template = document.getElementById("to-do__item-template");
 	const clone = template.content.querySelector(".to-do__item").cloneNode(true);
 	const textElement = clone.querySelector(".to-do__item-text");
-  //const deleteButton = clone.querySelector(".to-do__item-button_type_delete");
-  //const duplicateButton = clone.querySelector(".to-do__item-button_type_duplicate");
-  //const editButton = clone.querySelector(".to-do__item-button_type_edit");
+  	const deleteButton = clone.querySelector(".to-do__item-button_type_delete");
+  	//const duplicateButton = clone.querySelector(".to-do__item-button_type_duplicate");
+  	//const editButton = clone.querySelector(".to-do__item-button_type_edit");
 	textElement.textContent = item;
+
+	deleteButton.addEventListener("click", () => {
+		clone.remove();
+		const updatedTasks = getTasksFromDOM();
+		saveTasks(updatedTasks);
+	});
+
 	return clone;
 }
 
